a8fba46dd710cc737177f6b73c920a2c1151cdb1
ke_sx <ke_sx@qq.com>
2019-02-17 01:53:07 +0800

    v1.0

--- myapp/app.js
+++ myapp/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var socket_io = require('socket.io');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -37,5 +38,30 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.ready=function(server){
+  var io = socket_io.listen(server);
+  global.io = io;
+  io.on('connection',  function(socket) {
+    console.log('client connect server, ok!');
+  
+    // io.emit()方法用于向服务端发送消息，参数1表示自定义的数据名，参数2表示需要配合事件传入的参数
+    io.emit('server message', {msg:'client connect server success'});
+  
+    // socket.broadcast.emmit()表示向除了自己以外的客户端发送消息
+    socket.broadcast.emit('server message', {msg:'broadcast'});
+  
+    // 监听断开连接状态：socket的disconnect事件表示客户端与服务端断开连接
+    socket.on('disconnect', function() {
+      console.log('connect disconnect');
+    });
+    
+    // 与客户端对应的接收指定的消息
+    socket.on('client message', function(data) {
+      console.log(data);// hi server
+    });
+  
+  });
+};
 
 module.exports = app;
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/', function(req, res, next) {
+  console.log(req.body.data);
+  global.io.emit('map', {msg:req.body.data});
+  res.sendStatus(200);
+})
+
 
 module.exports = router;
